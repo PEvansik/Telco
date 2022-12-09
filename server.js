bd876ef4e84d5b4dc699c3676a8651d4551b3955
IKHARO Paul <paulevansikharo@gmail.com>
2022-12-09 14:22:40 +0100

    first Telco commit

---
+++ server.js
@@ -0,0 +1,78 @@
+const express = require('express')
+const path = require('path')
+const MongoClient = require('mongodb').MongoClient
+require ('dotenv').config()
+
+const app = express()
+
+let PORT = process.env.PORT || 3000;
+
+app.set("views", path.join(__dirname,'views'))
+app.set('view engine', 'ejs')
+
+
+app.use(express.static(path.join(__dirname,'public')))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+let db,
+    dbString = process.env.DB_STRING,
+    dbName = 'telcocustomers';
+
+
+MongoClient.connect(dbString, {useUnifiedTopology: true})
+    .then(client => {
+
+        console.log('connected to mongodb')
+        db = client.db(dbName);
+        let telcust = db.collection('telcust');
+
+        // ********** GET ************
+        // serve the home page
+
+        app.get('/', (req, res) => {
+            telcust.find().toArray()
+                .then(data => {
+                    console.log(data)
+                    res.render('index.ejs', {info: data})
+                })
+                .catch(err => console.error(err))
+        })
+
+        // ********** POST ************
+        // create customers
+
+        app.post('/details', (req,res) => {
+            telcust.insertOne(req.body)
+            .then(result => {
+
+                console.log(req.body)
+
+                res.redirect('/')
+            })
+            .catch(err => console.error(err))
+        })
+
+        app.put('/details', (req, res) => {
+            telcust.findOneAndUpdate(
+                {name: ""},
+                {$set : {
+                    name: req.body.name,
+                    number: req.body.number
+                }},
+                {
+                    upsert: true
+                }
+            )
+            console.log(req.body)
+            .then(result => console.log(result))
+            .catch()
+            // console.log(result)
+        })
+
+
+
+
+        app.listen(PORT, console.log(`Server running on port ${PORT}`))
+    })
+    .catch(err => console.error(err))
