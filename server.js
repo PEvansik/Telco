6bb0181892dbd5c6cd296ab6dc18f791e4f95acc
Alex Chadwick <alex.chadwick@mindprofessionals.com>
2022-12-09 19:50:12 +0000

    refactor: extracted logic to create and start a server to it's own function

--- server.js
+++ server.js
@@ -18,60 +18,54 @@ app.use(express.urlencoded({extended: true}))
 let db,
     dbString = process.env.DB_STRING,
     dbName = 'telcocustomers';
+    
+function createServer(mongodbClient) {
+    console.log('connected to mongodb')
+    db = mongodbClient.db(dbName);
+    let telcust = db.collection('telcust');
 
+    // ********** GET ************
+    // serve the home page
 
-MongoClient.connect(dbString, {useUnifiedTopology: true})
-    .then(client => {
-
-        console.log('connected to mongodb')
-        db = client.db(dbName);
-        let telcust = db.collection('telcust');
-
-        // ********** GET ************
-        // serve the home page
-
-        app.get('/', (req, res) => {
-            telcust.find().toArray()
-                .then(data => {
-                    console.log(data)
-                    res.render('index.ejs', {info: data})
-                })
-                .catch(err => console.error(err))
-        })
-
-        // ********** POST ************
-        // create customers
-
-        app.post('/details', (req,res) => {
-            telcust.insertOne(req.body)
-            .then(result => {
-
-                console.log(req.body)
-
-                res.redirect('/')
+    app.get('/', (req, res) => {
+        telcust.find().toArray()
+            .then(data => {
+                console.log(data)
+                res.render('index.ejs', {info: data})
             })
             .catch(err => console.error(err))
-        })
+    })
 
-        app.put('/details', (req, res) => {
-            telcust.findOneAndUpdate(
-                {name: ""},
-                {$set : {
-                    name: req.body.name,
-                    number: req.body.number
-                }},
-                {
-                    upsert: true
-                }
-            )
+    // ********** POST ************
+    // create customers
+
+    app.post('/details', (req,res) => {
+        telcust.insertOne(req.body)
+        .then(result => {
+
             console.log(req.body)
 
-            // console.log(result)
+            res.redirect('/')
         })
-
-
-
-
-        app.listen(PORT, console.log(`Server running on port ${PORT}`))
+        .catch(err => console.error(err))
     })
+
+    app.put('/details', (req, res) => {
+        telcust.findOneAndUpdate(
+            {name: ""},
+            {$set : {
+                name: req.body.name,
+                number: req.body.number
+            }},
+            {
+                upsert: true
+            }
+        );
+    });
+
+    app.listen(PORT, console.log(`Server running on port ${PORT}`))
+}
+
+MongoClient.connect(dbString, {useUnifiedTopology: true})
+    .then(createServer)
     .catch(err => console.error(err))
